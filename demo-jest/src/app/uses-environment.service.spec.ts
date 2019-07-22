43ec3f778a7622ad4de41b0d58801ca57170eeea
Georgi Parlakov <g.parlakov@propy.com>
2019-07-22 13:28:32 +0300

    make test fail

--- demo-jest/src/app/uses-environment.service.spec.ts
+++ demo-jest/src/app/uses-environment.service.spec.ts
@@ -2,13 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { UsesEnvironmentService } from './uses-environment.service';
 import {} from 'jest';
 
-jest.mock('../environments/environment.ts', () => {
-  return {
-    environment: {
-      production: true
-    }
-  };
-});
+// jest.mock('../environments/environment.ts', () => {
+//   return {
+//     environment: {
+//       production: true
+//     }
+//   };
+// });
 
 describe('UsesEnvironmentService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
